634f42fab27cfa2bf012e4dece899f7333b4569d
dongdaqing <dongdaqing@dongdaqing.local>
2015-06-04 15:33:58 +0800

    Unit and UI Testing in Android Studio

---
+++ app/src/main/java/com/example/testing/testingexample/Calculator.java
@@ -0,0 +1,24 @@
+package com.example.testing.testingexample;
+
+/**
+ * Created by on 15/6/4.
+ */
+public class Calculator {
+
+    public double sum(double a, double b){
+        return a + b;
+    }
+
+    public double substract(double a, double b){
+        return 0;
+    }
+
+    public double divide(double a, double b){
+        return 0;
+    }
+
+    public double multiply(double a, double b){
+        return 0;
+    }
+
+}
---
+++ app/src/test/java/com/example/testing/testingexample/CalculatorTest.java
@@ -0,0 +1,41 @@
+package com.example.testing.testingexample;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by on 15/6/4.
+ */
+public class CalculatorTest {
+
+    private Calculator mCalculator;
+
+    @Before
+    public void setUp() throws Exception {
+        mCalculator = new Calculator();
+    }
+
+    @Test
+    public void testSum() throws Exception {
+        //expected: 6, sum of 1 and 5
+        assertEquals(6d, mCalculator.sum(1d, 5d), 0);
+    }
+
+    @Test
+    public void testSubstract() throws Exception {
+        assertEquals(1d, mCalculator.substract(5d, 4d), 0);
+    }
+
+    @Test
+    public void testDivide() throws Exception {
+        assertEquals(4d, mCalculator.divide(20d, 5d), 0);
+    }
+
+    @Test
+    public void testMultiply() throws Exception {
+        assertEquals(10d, mCalculator.multiply(2d, 5d), 0);
+    }
+
+}
